52ac83ae17c7746843bef4145f59c4845ceffd16
olavhjemsaeter <olhje5765@oslomet.no>
2024-04-11 17:03:11 +0200

    RR

---
+++ src/main/java/com/example/forelesningsnotater/ForelesningsnotaterApplication.java
@@ -0,0 +1,13 @@
+package com.example.forelesningsnotater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForelesningsnotaterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForelesningsnotaterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/forelesningsnotater/ForelesningsnotaterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.forelesningsnotater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForelesningsnotaterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
